9e1a617bf814ef5b1e3ae3e5802fa884cacf7e73
ehowlsla <rupy1014@gmail.com>
2019-10-31 14:26:08 +0900

    example update

--- test.py
+++ test.py
@@ -1,27 +1,44 @@
 from lib.BootpayApi import BootpayApi
 # import requests
+# import json
+
 
 api = BootpayApi("5b9f51264457636ab9a07cde", 'sfilSOSVakw+PZA+PRux4Iuwm7a//9CXXudCq9TMDHk=', 'development')
+# api = BootpayApi("5b8f6a4d396fa665fdc2b5ea", 'n9jO7MxVFor3o//c9X5tdep95ZjdaiDvVB4h1B5cMHQ=')
+
 payload = {
     'price': 1000,
     'name': "pg test",
     'pg': 'inicis',
     'method': 'card',
+#     'application_id': '5b8f6a4d396fa665fdc2b5ea',
     'application_id': '5b9f51264457636ab9a07cde',
     'show_agree_window': 0,
-    'sms_payload': {},
-
+    'items': json.dumps([
+                {
+                    'item_name': 'i am item',
+                    'qty': 1,
+                    'unique': '123',
+                    'price': 1000,
+                    'cat1': 'TOP',
+                    'cat2': 'T-Shirt',
+                    'cat3': 'round T',
+                }
+    ]),
+    'user_info': json.dumps({
+        'username': 'user name',
+        'email': 'user email',
+        'addr': 'user address',
+        'phone': '010-1234-4567'
+    }),
     'order_id': 'order_id_1234',
-    'params': {
+    'params': json.dumps({
         'callback1': 'return value 1',
         'callback2': 'return value 2',
         'customvar1234': 'custom value 1234'
-    }
+    })
 }
+
 
 result = api.remote_link(payload)
 print(result)
-
-
-# print api.confirm('593f8febe13f332431a8ddae').text
-# print api.cancel('593f8febe13f332431a8ddae', 'name', 'reason').text
