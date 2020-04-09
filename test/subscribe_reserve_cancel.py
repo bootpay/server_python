393b89823e45837efab7b3389e45ffb180ad7795
Gosomi <gosomi@forelf.com>
2020-04-09 16:12:38 +0900

    예제 좀더 보완해서 추가

--- test/subscribe_reserve_cancel.py
+++ test/subscribe_reserve_cancel.py
@@ -11,14 +11,15 @@ bootpay = BootpayApi('59bfc738e13f337dbd6ca48a', 'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/C
 result = bootpay.get_access_token()
 if result['status'] is 200:
     reserve_result = bootpay.subscribe_billing_reserve(
-        '5b025b33e13f33310ce56fb',
+        '5e8d3c6f05df0f036ad43e41',
         '정기 결제 테스트 아이템',
         3000,
         '12345',
         time.time() + 1000,
         'https://dev-api.bootpay.co.kr/callback'
     )
+    print(reserve_result)
     if reserve_result['status'] is 200:
         response = bootpay.subscribe_billing_reserve_cancel(reserve_result['data']['reserve_id'])
         if response['status'] is 200:
-            print("성공")
+            print(response)
