bc41c4206b9a109d6d07fb8e1eba2153ecb36524
ehowlsla <ehowlsla@bootpay.co.kr>
2021-08-05 08:54:47 +0900

    test code update

--- test/remote_form.py
+++ test/remote_form.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from lib.BootpayApi import BootpayApi
 
-bootpay = BootpayApi('5b9f51264457636ab9a07cde', 'sfilSOSVakw+PZA+PRux4Iuwm7a//9CXXudCq9TMDHk=', 'development')
+bootpay = BootpayApi('5b8f6a4d396fa665fdc2b5ea', 'rm6EYECr6aroQVG2ntW0A6LpWnkTgP4uQ3H18sDDUYw=')
 
 # result = bootpay.get_access_token()
 # if result['status'] is 200:
--- test/subscribe.py
+++ test/subscribe.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from lib.BootpayApi import BootpayApi
 
-bootpay = BootpayApi('59bfc738e13f337dbd6ca48a', 'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=', 'development')
+bootpay = BootpayApi('5b8f6a4d396fa665fdc2b5ea', 'rm6EYECr6aroQVG2ntW0A6LpWnkTgP4uQ3H18sDDUYw=')
 
 result = bootpay.get_access_token()
 if result['status'] is 200:
